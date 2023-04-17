5f9ac86961429f277392dad00b30986cdf5e80d2
mtzcpd1346 <eduardodonascimento@cvccorp.com.br>
2023-04-17 14:50:21 -0300

    sem front

--- backup_mongo.sh
+++ backup_mongo.sh
@@ -3,11 +3,11 @@
 # Script de backup diário do volume mongodb_data_container
 
 # Verifica se o diretório de backup existe e cria se necessário
-if [ ! -d "/home/amacoon/deploy/backup" ]; then
-  mkdir /home/amacoon/deploy/backup
+if [ ! -d "/home/amacoon/amacoondocker/backup" ]; then
+  mkdir /home/amacoon/amacoondocker/backup
 fi
 
 # Faz o backup do volume mongodb_data_container
 echo "Iniciando o backup do volume mongodb_data_container..."
-docker run --rm -v mongodb_data_container:/data/db -v /home/amacoon/deploy/backup:/backup ubuntu /bin/bash -c "tar czf /backup/mongodb_data_backup.tar.gz /data/db"
-echo "Backup concluído com sucesso. Arquivo de backup salvo em /home/amacoon/deploy/backup/mongodb_data_backup.tar.gz"
+docker run --rm -v mongodb_data_container:/data/db -v /home/amacoon/amacoondocker/backup:/backup ubuntu /bin/bash -c "tar czf /backup/mongodb_data_backup.tar.gz /data/db"
+echo "Backup concluído com sucesso. Arquivo de backup salvo em /home/amacoon/amacoondocker/backup/mongodb_data_backup.tar.gz"
